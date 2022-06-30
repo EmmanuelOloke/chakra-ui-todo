d9820b05cf48870912e915747d6b35c730ac6fa8
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-30 04:08:40 +0100

    created components and imported VStack from chakra-ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react';
+
 import { Heading } from '@chakra-ui/react';
+import { VStack } from '@chakra-ui/react';
+
+import TodoList from './components/TodoList';
+import AddTodo from './components/AddTodo';
 
 function App() {
-  return <Heading>Todo Application</Heading>;
+  return (
+    <VStack>
+      <Heading>Todo Application</Heading>
+      <TodoList>Somthing in here</TodoList>
+      <AddTodo>Another thing in here</AddTodo>
+    </VStack>
+  );
 }
 
 export default App;
