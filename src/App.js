39b0e55d25a4c80a09e10e038ae59022d9ce6ab7
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-06-30 04:27:42 +0100

    installed react-icons and styled IconButton and heading

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React from 'react';
 
 import { Heading } from '@chakra-ui/react';
-import { VStack } from '@chakra-ui/react';
+import { VStack, IconButton } from '@chakra-ui/react';
+
+import { FaSun, FaMoon } from 'react-icons/fa';
 
 import TodoList from './components/TodoList';
 import AddTodo from './components/AddTodo';
 
 function App() {
   return (
-    <VStack>
-      <Heading>Todo Application</Heading>
+    <VStack p={4}>
+      <IconButton
+        icon={<FaSun />}
+        isRound="true"
+        size="lg"
+        alignSelf="flex-end"
+      />
+      <Heading
+        mb="8"
+        fontWeight="extrabold"
+        size="2xl"
+        bgGradient="linear(to-r, pink.500, pink.300, blue.500)"
+        bgClip="text"
+      >
+        Todo Application
+      </Heading>
       <TodoList>Somthing in here</TodoList>
       <AddTodo>Another thing in here</AddTodo>
     </VStack>
